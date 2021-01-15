44323a47c9abddf9d037e626f0aead095a02b345
AlexVV82 <mwbinua@gmail.com>
2021-01-15 17:24:24 +0200

    JHW 11 1 COLOR

---
+++ script.js
@@ -0,0 +1,48 @@
+import {colors} from "./colors.js"
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body')
+}
+// console.log(refs.body);
+// console.log(refs.startBtn);
+
+refs.startBtn.addEventListener('click', () => { changeBackgroundColor.start() })
+refs.stopBtn.addEventListener('click', () => { changeBackgroundColor.stop() })
+
+
+console.log(colors[0])
+
+const changeBackgroundColor = {
+  getNewColorId: null,
+  isActive: false,
+  
+
+  start() {
+
+    if(this.isActive) {
+    return
+    }   
+    
+    refs.body.style.backgroundColor = '#ffffff'
+    this.isActive = true;
+    this.getNewColorId = setInterval(() => {
+      
+      refs.body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`
+     
+      
+        }, 1000);
+},
+
+    stop() {
+      clearInterval(this.getNewColorId)
+      this.isActive = false
+    }
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+
+};
+
